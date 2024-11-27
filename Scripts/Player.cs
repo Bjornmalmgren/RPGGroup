88b8ae68125524c7c3c6d2f624bebbf8a0d58d7c
Dennis Karlsson <dk.karlsson04@gmail.com>
2024-11-27 12:12:54 +0100

    Player merged

---
+++ Scripts/Player.cs
@@ -0,0 +1,51 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody2D
+{
+	[Export]
+	private  float movementSpeed = 300.0f;
+
+	[Export]
+	public int health;
+
+
+	
+	public void Move()
+	{
+		Vector2 inputDir = Input.GetVector("Left", "Right", "Up", "Down");
+		if (Input.IsActionJustPressed("Left", false))
+		{
+
+			Scale = new Vector2(1, -1);
+			RotationDegrees = -180f;
+            GD.Print(Scale);
+        }
+		if (Input.IsActionJustPressed("Right", false))
+		{
+            Scale = new Vector2(1, 1);
+            RotationDegrees = 0f;
+            GD.Print(Scale);
+        }
+		//GD.Print(Scale);
+		Velocity = inputDir * movementSpeed;
+	}
+	public void OnDeath()
+	{
+
+	}
+	public void Attack()
+	{
+
+	}
+	public override void _PhysicsProcess(double delta)
+	{
+		Move();
+		MoveAndSlide();
+		if (health <=0)
+		{
+			OnDeath();
+		}
+	}
+	
+}
